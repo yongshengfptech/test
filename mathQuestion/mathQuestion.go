011a6d2c9ca88f81c46a3f12031ce19eb75e88df
yongshengfptech <yongsheng.fptech@gmail.com>
2023-06-03 16:33:22 +0800

    add a package

---
+++ mathQuestion/mathQuestion.go
@@ -0,0 +1,40 @@
+package mathQuestion
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+type question struct {
+	Num1     int
+	Num2     int
+	Operator rune
+	Ans      int
+}
+
+func RandomQuestion() question {
+	num1 := rand.Intn(20)
+	num2 := rand.Intn(20)
+
+	operators := []rune{'+', '-'}
+	operator := operators[rand.Intn(len(operators))]
+
+	var ans int
+	if operator == '+' {
+		ans = num1 + num2
+	} else if operator == '-' {
+		if num1 < num2 {
+			num1, num2 = num2, num1
+		}
+		ans = num1 - num2
+	}
+
+	return question{Num1: num1, Num2: num2, Operator: operator, Ans: ans}
+}
+
+func CheckAns(ans int, q question) string {
+	if ans == q.Ans {
+		return fmt.Sprintln("Correct Answer")
+	}
+	return fmt.Sprintln("Wrong Answer")
+}
