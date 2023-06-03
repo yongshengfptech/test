f0c5161c7d332b3f58ad811e20d296f07bcd84e6
yongshengfptech <yongsheng.fptech@gmail.com>
2023-06-03 18:37:14 +0800

    Update

--- test.go
+++ test.go
@@ -11,18 +11,18 @@ func main() {
 
 func QandA() {
 	q := mathQuestion.RandomQuestion()
-	fmt.Println("\nEnter -1 to exit.")
+	fmt.Println("Enter -1 to exit.")
 	fmt.Printf("%v %c %v = ", q.Num1, q.Operator, q.Num2)
 
 	var ans int
-	_, err := fmt.Scanf("%d", &ans)
+	_, err := fmt.Scanf("%d\n", &ans)
 	if err == nil {
 		if ans == -1 {
 			return
 		}
 		fmt.Printf("%s\n", mathQuestion.CheckAns(ans, q))
 	} else {
-		fmt.Println("Not a number")
+		fmt.Printf("Not a number\n\n")
 	}
 	QandA()
 }
