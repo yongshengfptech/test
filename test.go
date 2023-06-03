ceaaac5636562a47932bb9f475be7c10fa844b12
yongshengfptech <yongsheng.fptech@gmail.com>
2023-06-03 17:52:50 +0800

    Update

--- test.go
+++ test.go
@@ -1,11 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"project/test/mathQuestion"
+)
 
 func main() {
-	fmt.printf("1 + 2 = %v\n", sum(1, 2))
-}
+	q := mathQuestion.RandomQuestion()
+	fmt.Printf("%v %c %v = ", q.Num1, q.Operator, q.Num2)
 
-func sum(a, b int) int {
-	return a + b
+	var ans int
+	_, err := fmt.Scanf("%d", &ans)
+	if err != nil {
+		fmt.Println("\nNot a number")
+	} else {
+		fmt.Printf("\n%s", mathQuestion.CheckAns(ans, q))
+	}
 }
