de3c54256e8aa7be8c8102a011de268328088914
sun <chinsunkang.fptech@gmail.com>
2023-06-09 10:02:45 +0800

    update file

---
+++ api_test/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"api_test/config"
+	"api_test/database"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	app := fiber.New(fiber.Config{
+		BodyLimit: 500 * 1024 * 1024,
+	})
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept, Authrorization",
+	}))
+
+	config.Load_env()
+	database.Initialize()
+}
+
+// ==========================================
+
+// type event struct {
+// 	ID          string `json:"ID"`
+// 	Title       string `json:"title"`
+// 	Description string `json:"description"`
+// }
+
+// type all_event []event
+
+// var events = all_event{
+// 	{
+// 		ID:          "1",
+// 		Title:       "Hello",
+// 		Description: "hello hello",
+// 	},
+// }
+
+// func create_event(w http.ResponseWriter, r *http.Request) {
+// 	var new_event event
+// 	reqBody, err := ioutil.ReadAll(r.Body)
+// 	if err != nil {
+// 		fmt.Fprintf(w, "Error of request body")
+// 	}
+
+// 	json.Unmarshal(reqBody, &new_event)
+// 	events = append(events, new_event)
+// 	w.WriteHeader(http.StatusCreated)
+
+// 	json.NewEncoder(w).Encode(new_event)
+// }
+
+// func ServerHTTP(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hello world !!")
+// }
+
+// func main() {
+// 	fmt.Println(events)
+// 	router := mux.NewRouter().StrictSlash(true)
+// 	router.HandleFunc("/", ServerHTTP)
+// 	router.HandleFunc("/create_event", create_event).Methods("POST")
+// 	log.Fatal(http.ListenAndServe(":8080", router))
+
+// }
